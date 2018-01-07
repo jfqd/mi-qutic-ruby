4680617057cdf133385692812719f16578eb630b
jfqd <jfqd@blun.org>
2018-01-07 13:32:21 +0100

    add more metadata

--- copy/opt/qutic/var/mdata-setup/includes/54-ruby.sh
+++ copy/opt/qutic/var/mdata-setup/includes/54-ruby.sh
@@ -1,17 +1,7 @@
-# set data path
-if mdata-get data_path 1>/dev/null 2>&1; then
-  DATA_PATH=`mdata-get data_path`
-  cd /var/www/htdocs/nextcloud/current/
-  ln -nfs "$DATA_PATH" data
-fi
-
 if mdata-get server_name 1>/dev/null 2>&1; then
   SERVER_NAME=`mdata-get server_name`
   sed -i "s:SERVER_NAME:$SERVER_NAME:g" /opt/local/etc/httpd/vhosts/01-ruby.conf
 fi
-
-MAXINSTANCES
-WORKERS
 
 if mdata-get server_name 1>/dev/null 2>&1; then
   SERVER_ALIAS=`mdata-get server_alias`
@@ -19,6 +9,18 @@ if mdata-get server_name 1>/dev/null 2>&1; then
 else
   sed -i "s:ServerAlias SERVER_ALIAS::g" /opt/local/etc/httpd/vhosts/01-ruby.conf
 fi
+
+MAXINSTANCES=10
+if mdata-get passenger_maxinstances 1>/dev/null 2>&1; then
+  MAXINSTANCES=`mdata-get passenger_maxinstances`
+fi
+sed -i "s:MAXINSTANCES:$MAXINSTANCES:g" /opt/local/etc/httpd/vhosts/01-ruby.conf
+
+WORKERS=4
+if mdata-get passenger_workers 1>/dev/null 2>&1; then
+  WORKERS=`mdata-get passenger_workers`
+fi
+sed -i "s:WORKERS:$WORKERS:g" /opt/local/etc/httpd/vhosts/01-ruby.conf
 
 # Enable apache by default
 /usr/sbin/svcadm enable svc:/pkgsrc/apache:default
